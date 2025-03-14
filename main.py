6a6445fac6fb146cecfcc72e33118c2fd261fd11
Stojoc Vladimir <61602995+tokyo-s@users.noreply.github.com>
2025-03-14 13:43:02 +0200

    smol fix

--- main.py
+++ main.py
@@ -139,4 +139,4 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
 
     # Run the server
-    uvicorn.run("anki_fastapi:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
